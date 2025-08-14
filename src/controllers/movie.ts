ffbba213c003cdceaf2e8c584f45667a3a61af06
mukeshgeetha46 <mukeshgeetha46@gmail.com>
2025-08-15 00:38:17 +0530

    add moview controleer

--- src/controllers/movie.ts
+++ src/controllers/movie.ts
@@ -5,20 +5,6 @@ import db from "../database/db";
 import httpstatus from "../utils/httpstatus";
 import validator from "../utils/validator";
 
-
-
-
-interface User {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    otp?: string;
-    otp_verified?: boolean;
-    is_active?: boolean;
-    [key: string]: any;
-}
-
 class MovieController {
     public FetchAllmovie = async (req: Request, res: Response): Promise<void> => {
         try {
@@ -30,6 +16,39 @@ class MovieController {
             
         }
     };
+    public FetchmovieDetails = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const { movie_id } = req.params;
+           const fetchmoivesdetails = await db('movies').select('*').where({ movie_id:movie_id }).first();
+
+            httpstatus.successResponse(fetchmoivesdetails, res);
+        } catch (e) {
+            console.error(e);
+            
+        }
+    };
+    public FetchTheaters = async (req: Request, res: Response): Promise<void> => {
+        try {
+           const fetchtheaters = await db('theaters').select('*');
+
+            httpstatus.successResponse(fetchtheaters, res);
+        } catch (e) {
+            console.error(e);
+            
+        }
+    };
+    public FetchTheaterSeats = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const { theater_id } = req.params;
+           const fetchtheaters = await db('seats').select('*').where({ theater_id:theater_id });
+
+            httpstatus.successResponse(fetchtheaters, res);
+        } catch (e) {
+            console.error(e);
+            
+        }
+    };
+
 
     
 
--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // Auth Routes
 router.get("/allmovies", MovieController.FetchAllmovie);
+router.get("/movies/:movie_id", MovieController.FetchmovieDetails);
+router.get("/theaters", MovieController.FetchTheaters);
+router.get("/theaters/:theater_id", MovieController.FetchTheaterSeats);
 
 
 export default router;
